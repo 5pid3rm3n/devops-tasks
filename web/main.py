795114127a7f015050dbea02b4eb6245d138df7c
ita <itamartesting@gmail.com>
2022-03-12 07:33:15 +0200

    main.py: adding api_consumer module

--- web/main.py
+++ web/main.py
@@ -1,6 +1,7 @@
-import random
 from fastapi import FastAPI
-from web.producer import producer, on_send_success, on_send_error
+from web.producer import producer
+from web.api_consumer import consume
+import random
 
 random_items = {'Shoes': 179, 'Bicycle': 1099, 'Surfboard': 755,
                 'Hat': 43, 'Watch': 230, 'Laptop': 3700}
@@ -14,15 +15,16 @@ async def buy_item():
 
     # get random item
     item, price = random.choice(list(random_items.items()))
-    
-    # produce new item to kafka
-    prodocer.send('my-topic', {item: price}).add_callback(on_send_success).add_errback(on_send_error)
 
-    # return item purchased  
+    # produce new item to kafka
+    prodocer.send('my-topic', {item: price})
+
+    # return item purchased
     return {item: price}
 
 
 @app.get("/get-all-user_buys/")
 async def get_all_user_buys():
 
-    return 'user_buys'
+    user_buys = consume()
+    return user_buys
