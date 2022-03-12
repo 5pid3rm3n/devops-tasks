108264e2f9dfe8e6dd070bb872aca619c29a76b5
ita <itamartesting@gmail.com>
2022-03-12 17:35:40 +0200

    main.py: chacnge produce payload content

--- web/main.py
+++ web/main.py
@@ -1,10 +1,14 @@
+from datatime import datatime
 from fastapi import FastAPI
 from web.producer import producer
 from web.api_consumer import consume
 import random
 
+
 random_items = {'Shoes': 179, 'Bicycle': 1099, 'Surfboard': 755,
                 'Hat': 43, 'Watch': 230, 'Laptop': 3700}
+data = {'username': 'Shopper', 'userid': '00001'}
+
 
 app = FastAPI()
 prodocer = producer()
@@ -14,13 +18,16 @@ prodocer = producer()
 async def buy_item():
 
     # get random item
-    item, price = random.choice(list(random_items.items()))
+    data.item, data.price = random.choice(list(random_items.items()))
+    
+    # set timestamp
+    data.timestamp = str(datatime.utcnow())
 
     # produce new item to kafka
-    prodocer.send('my-topic', {item: price})
+    prodocer.send('my-topic', data)
 
     # return item purchased
-    return {item: price}
+    return data
 
 
 @app.get("/get-all-user_buys/")
